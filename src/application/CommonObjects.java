05347c903b4fdbf8a346b8c1147acd77b6ff248f
Satyansh Rai <srai109@gmail.com>
2023-10-25 17:54:31 -0700

    Completed initial implementation of create ticket menu with some improvements to overall code.

--- src/application/CommonObjects.java
+++ src/application/CommonObjects.java
@@ -1,6 +1,18 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class CommonObjects {
 
@@ -21,6 +33,38 @@ public class CommonObjects {
 	public void setMainBox(HBox mainBox) {
 		this.mainBox = mainBox;
 	}
-	
+
+	public List<String> readProjectNames(String filePath) {
+		List<String> projectNames = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                if (parts.length >= 1) {
+                    String projectName = parts[0].replaceAll("-", "|");
+                    projectNames.add(projectName);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return projectNames;
+	}
+
+	public void backToMainMenu(ActionEvent event, Stage stage, Scene scene, Parent root) {
+		try {
+			root = FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
---
+++ src/application/controller/CreateTicketController.java
@@ -0,0 +1,42 @@
+package application.controller;
+
+import java.util.List;
+import application.CommonObjects;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+
+public class CreateTicketController{
+
+	private CommonObjects commonObjects = CommonObjects.getInstance();
+	@FXML AnchorPane ticketMenuBox;
+	@FXML TextField ticketName;
+	@FXML TextArea ticketDescription;
+	@FXML ChoiceBox<String> projectBox;
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	@FXML public void initialize() {
+		String filePath = "./data/saved-projects.csv"; // Provide the path to your CSV file
+        List<String> projectNames = commonObjects.readProjectNames(filePath);
+        projectNames.sort(null);
+        projectBox.getItems().addAll(projectNames);
+	}
+	
+	@FXML public void confirmButtonPressedOP(ActionEvent event) {
+		commonObjects.backToMainMenu(event, stage, scene, root);
+	}
+	
+	@FXML public void backButtonPressedOP(ActionEvent event) {
+		commonObjects.backToMainMenu(event, stage, scene, root);
+	}
+	
+	
+}
