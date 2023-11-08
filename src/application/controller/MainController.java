57667e07ec712315462cf693e4d4133366cdafcf
anchalmandavia <anchal.mandavia@gmail.com>
2023-11-07 23:33:02 -0800

    Migrate csv - SQLite. Create and Store Project

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -11,98 +11,102 @@ import javafx.scene.layout.HBox;
 
 public class MainController {
 
-	@FXML HBox mainBox;
-
-
-	@FXML public void createProjectOp() {
-		
-		URL url = getClass().getClassLoader().getResource("view/CreateProject.fxml");
-		
-		try {
-			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
-			
-			if (mainBox.getChildren().size() > 0)
-				mainBox.getChildren().remove(0);
-				
-			mainBox.getChildren().add(pane1);
-		} 
-		
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@FXML public void showProjectsOp() {
-		URL url = getClass().getClassLoader().getResource("view/ShowProjects.fxml");
-    
-		try {
-			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
-			
-			if (mainBox.getChildren().size() > 0)
-				mainBox.getChildren().remove(0);
-				
-			mainBox.getChildren().add(pane1);
-		} 
-		
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@FXML public void showTicketsOp() {
-		URL url = getClass().getClassLoader().getResource("view/ShowTickets.fxml");
-    
-		try {
-			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
-			
-			if (mainBox.getChildren().size() > 0)
-				mainBox.getChildren().remove(0);
-				
-			mainBox.getChildren().add(pane1);
-		} 
-		
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@FXML public void createTicketOP() {
-		URL url = getClass().getClassLoader().getResource("view/CreateTicket.fxml");
-		
-		try {
-			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
-			
-			if (mainBox.getChildren().size() > 0)
-				mainBox.getChildren().remove(0);
-				
-			mainBox.getChildren().add(pane1);
-		} 
-		
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	@FXML
-    public void createCommentOP() {
-        URL url = getClass().getClassLoader().getResource("view/CreateComment.fxml");
+	HBox mainBox;
 
-        try {
-            AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+	@FXML
+	public void createProjectOp() {
 
-            if (mainBox.getChildren().size() > 0)
-                mainBox.getChildren().remove(0);
+		URL url = getClass().getClassLoader().getResource("view/CreateProject.fxml");
 
-            mainBox.getChildren().add(pane1);
-        }
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
 
-        catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+			if (mainBox.getChildren().size() > 0)
+				mainBox.getChildren().remove(0);
+
+			mainBox.getChildren().add(pane1);
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void showProjectsOp() {
+		URL url = getClass().getClassLoader().getResource("view/ShowProjects.fxml");
+
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+
+			if (mainBox.getChildren().size() > 0)
+				mainBox.getChildren().remove(0);
+
+			mainBox.getChildren().add(pane1);
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void showTicketsOp() {
+		URL url = getClass().getClassLoader().getResource("view/ShowTickets.fxml");
+
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+
+			if (mainBox.getChildren().size() > 0)
+				mainBox.getChildren().remove(0);
+
+			mainBox.getChildren().add(pane1);
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void createTicketOP() {
+		URL url = getClass().getClassLoader().getResource("view/CreateTicket.fxml");
+
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+
+			if (mainBox.getChildren().size() > 0)
+				mainBox.getChildren().remove(0);
+
+			mainBox.getChildren().add(pane1);
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void createCommentOP() {
+		URL url = getClass().getClassLoader().getResource("view/CreateComment.fxml");
+
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+
+			if (mainBox.getChildren().size() > 0)
+				mainBox.getChildren().remove(0);
+
+			mainBox.getChildren().add(pane1);
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
