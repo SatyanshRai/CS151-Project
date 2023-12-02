1c74f2e6d3a90b58f2af99b40db081290d186f91
Satyansh Rai <srai109@gmail.com>
2023-12-01 19:55:32 -0800

    Added edit and delete functionality to comments.

--- src/application/controller/ShowCommentsController.java
+++ src/application/controller/ShowCommentsController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import application.CommonObjects;
 import application.Main;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,8 +22,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import java.sql.*;
+import java.time.LocalDate;
+
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
@@ -33,6 +37,10 @@ public class ShowCommentsController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	PreparedStatement pst;
+    ResultSet rs;
+    Connection conn;
+
 	
 	@FXML private ChoiceBox<String> projectChoiceBox;
 	@FXML private ChoiceBox<String> ticketChoiceBox;
@@ -41,12 +49,16 @@ public class ShowCommentsController {
 	private TableView<Comment> commentTableView;
 	private ObservableList<Comment> tableData = FXCollections.observableArrayList();
 	
-	@FXML private TableColumn ticketCol;
-	@FXML private TableColumn projectCol;
-	@FXML private TableColumn commentCol;
+	@FXML private TableColumn<Comment, String> ticketCol;
+	@FXML private TableColumn<Comment, String> projectCol;
+	@FXML private TableColumn<Comment, String> commentCol;
+	@FXML private TableColumn<Comment, String> dateCol;
+
 	
 	String projectPath;
 	String ticketPath;
+	
+    Integer index;
 	
 	List<String> allProjectNames;
 	List<String> allTicketNames;
@@ -74,6 +86,8 @@ public class ShowCommentsController {
 		ticketCol.setCellValueFactory(new PropertyValueFactory<>("ticketName"));
 		projectCol.setCellValueFactory(new PropertyValueFactory<>("projectName"));
 		commentCol.setCellValueFactory(new PropertyValueFactory<>("commentDescription"));
+		dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+
 		
 		// Listen for changes in project selection
 		projectChoiceBox.getSelectionModel().selectedItemProperty().addListener(
@@ -103,13 +117,19 @@ public class ShowCommentsController {
 					}
 				});
 		
-		commentTableView.getSelectionModel().selectedItemProperty().addListener(
-				(observable, oldValue, newValue) -> {
-					commentDescDisplay.setText(newValue.getCommentDescription());
-				});
-		
 		displayAllComments();	
 	}
+	
+	@FXML
+    void getItem(MouseEvent event) {
+        index = commentTableView.getSelectionModel().getSelectedIndex();
+
+        if (index <= -1) {
+            return;
+        }
+
+        commentDescDisplay.setText(commentCol.getCellData(index).toString());
+    }
 	
 	public void displayAllComments() {
 		tableData.clear();
@@ -142,8 +162,10 @@ public class ShowCommentsController {
 			allTicketNames = readTicketsForProjectFromDatabase(allProjectNames.get(i));
 			for (int j = 0; j < allTicketNames.size(); j++) {
 				List<String> commentTexts = readCommentsForTicketFromDatabase(allTicketNames.get(j));
+				List<String> commentDates = readCommentsDateForTicketFromDatabase(allTicketNames.get(j));
+				List<Integer> commentIDs = readCommentsIDForTicketFromDatabase(allTicketNames.get(j));
 				for (int k = 0; k < commentTexts.size(); k++) {
-					allComments.add(new Comment(commentTexts.get(k), allTicketNames.get(j), allProjectNames.get(i)));
+					allComments.add(new Comment(commentTexts.get(k), allTicketNames.get(j), allProjectNames.get(i), commentDates.get(k), commentIDs.get(k)));
 				}
 			}
 		}
@@ -232,6 +254,60 @@ public class ShowCommentsController {
 		
 		return commentData;
 	}
+	
+	public List<String> readCommentsDateForTicketFromDatabase(String selectedTicket) {
+		List<String> commentDates = new ArrayList<String>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			//Obtain the connection from the Main class
+			connection = Main.getConnection();
+			
+			//SQL query to fetch comment titles for the selected ticket
+			String sql = "SELECT comment_date FROM Comments WHERE ticket_id = (SELECT ticket_id FROM Tickets WHERE ticket_title = ?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, selectedTicket);
+			resultSet = preparedStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				String commentDate = resultSet.getString("comment_date");
+				commentDates.add(commentDate);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return commentDates;
+	}
+	
+	public List<Integer> readCommentsIDForTicketFromDatabase(String selectedTicket) {
+		List<Integer> commentIDs = new ArrayList<Integer>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			//Obtain the connection from the Main class
+			connection = Main.getConnection();
+			
+			//SQL query to fetch comment titles for the selected ticket
+			String sql = "SELECT comment_id FROM Comments WHERE ticket_id = (SELECT ticket_id FROM Tickets WHERE ticket_title = ?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, selectedTicket);
+			resultSet = preparedStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				int commentID = resultSet.getInt("comment_id");
+				commentIDs.add(commentID);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return commentIDs;
+	}
 
 	@FXML
 	public void backFromCreateProjectOp(ActionEvent event) {
@@ -282,11 +358,15 @@ public class ShowCommentsController {
 		private final SimpleStringProperty commentDescription;
 		private final SimpleStringProperty ticketName;
 		private final SimpleStringProperty projectName;
+		private final SimpleStringProperty cDate;
+		private final SimpleIntegerProperty cID;
 		
-		private Comment(String commentDescription, String ticketName, String projectName) {
+		private Comment(String commentDescription, String ticketName, String projectName, String cDate, int cID) {
 			this.commentDescription = new SimpleStringProperty(commentDescription);
 			this.ticketName = new SimpleStringProperty(ticketName);
 			this.projectName = new SimpleStringProperty(projectName);
+			this.cDate = new SimpleStringProperty(cDate);
+			this.cID = new SimpleIntegerProperty(cID);
 		}
 		
 		public String getCommentDescription() {
@@ -300,5 +380,77 @@ public class ShowCommentsController {
 		public String getProjectName() {
 			return projectName.get();
 		}
+		
+		public String getDate() {
+			return cDate.get();
+		}
+		public int getID() {
+			return cID.get();
+		}
 	}
+
+	@FXML public void saveChangesOP() {
+		index = commentTableView.getSelectionModel().getSelectedIndex();
+		conn = Main.getConnection();
+
+        if (index >= 0) {
+            try {
+                String commentDescription = commentDescDisplay.getText();
+                String date;
+                date = LocalDate.now().toString();
+    			String updateQuery = "UPDATE Comments SET comment_text=?, comment_date=? WHERE comment_id=?";
+    			String currTicket = ticketCol.getCellData(index).toString();
+                String currProject = projectCol.getCellData(index).toString();
+                int currID = tableData.get(index).getID();
+                pst = conn.prepareStatement(updateQuery);
+                pst.setString(1, commentDescription);
+                pst.setString(2, date);
+                pst.setInt(3, currID);
+                pst.executeUpdate();
+
+                Comment updatedItem = new Comment(commentDescription, currTicket, currProject, date, currID);
+                commentTableView.getItems().set(index, updatedItem);
+                tableData.clear();
+                repopulate();
+                searchButtonOP();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+	}
+
+	@FXML public void deleteCommentOP() {
+		index = commentTableView.getSelectionModel().getSelectedIndex();
+		conn = Main.getConnection();
+		
+        if (index >= 0) {
+            try {
+                String commentText = commentCol.getCellData(index).toString();
+
+                String deleteQuery = "DELETE FROM Comments WHERE comment_text = ?";
+                pst = conn.prepareStatement(deleteQuery);
+                pst.setString(1, commentText);
+                pst.executeUpdate();
+
+                commentTableView.getItems().remove(index);
+                commentDescDisplay.clear();
+                
+                tableData.clear();
+                repopulate();
+                searchButtonOP();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+	}
+	private void repopulate() {
+		allProjectNames = readProjectNamesFromDatabase();
+		generateCommentObjects();
+		
+		commentTableView.setItems(tableData);
+		ticketCol.setCellValueFactory(new PropertyValueFactory<>("ticketName"));
+		projectCol.setCellValueFactory(new PropertyValueFactory<>("projectName"));
+		commentCol.setCellValueFactory(new PropertyValueFactory<>("commentDescription"));
+		dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+    }
 }
